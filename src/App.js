f423387aab398beb0d6cba97e4c7f4bb1081bede
nunnnunputri-prog <nunnnunputri@example.com>
2026-06-16 19:06:42 +0700

    update: ubah teks dan hapus logo react

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>🚀 CI/CD VERCEL SUKSES!</h1>
+        <h1>CI/CD VERCEL SUKSES!</h1>
         <p>Setiap push ke main auto deploy</p>
       </header>
     </div>
