05d205dc1315f0be826b27c043f31d6147df4af5
nunnnunputri-prog <nunnnunputri@example.com>
2026-06-16 19:12:44 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>CI/CD VERCEL SUKSES!</h1>
-        <p>Setiap push ke main auto deploy</p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
